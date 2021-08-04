33b1bca89212383d3fe1ea41f883dc580688687d
Jerry <306722860@qq.com>
2021-08-04 14:56:59 +0800

    Initilized...

---
+++ LearnOpenGLAAA/premake5.lua
@@ -0,0 +1,36 @@
+-- OpenGL-Sandbox
+workspace "LearnOpenGLAAA"
+	architecture "x64"
+	startproject "OpenGL-Sandbox"
+
+	configurations
+	{
+		"Debug",
+		"Release"
+	}
+	
+	flags
+	{
+		"MultiProcessorCompile"
+	}
+
+outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+-- Include directories relative to OpenGL-Core
+IncludeDir = {}
+IncludeDir["GLFW"] = "vendor/GLFW/include"
+IncludeDir["Glad"] = "vendor/Glad/include"
+IncludeDir["ImGui"] = "vendor/imgui"
+IncludeDir["glm"] = "vendor/glm"
+IncludeDir["stb_image"] = "vendor/stb_image"
+
+-- Projects
+group "Dependencies"
+	include "OpenGL-Core/vendor/GLFW"
+	include "OpenGL-Core/vendor/Glad"
+	include "OpenGL-Core/vendor/imgui"
+group ""
+
+include "OpenGL-Core"
+include "OpenGL-Sandbox"
+include "OpenGL-Examples"
---
+++ scripts/Win-Premake.bat
@@ -0,0 +1,6 @@
+@echo off
+
+pushd ..\LearnOpenGLAAA\
+call ..\vendor\bin\premake\premake5.exe vs2019
+popd
+PAUSE
