f5db71a5313ecf774d094b3e014cf9f05337a280
Jerry <306722860@qq.com>
2021-08-09 11:02:38 +0800

    Index Buffer Object(IBO)

--- LearnOpenGLAAA/02hello_triangle/src/SandboxLayer.cpp
+++ LearnOpenGLAAA/02hello_triangle/src/SandboxLayer.cpp
@@ -21,19 +21,30 @@ void SandboxLayer::OnAttach()
 		"assets/shaders/fs.frag.glsl"
 	);
 
+	// set up vertex data (and buffer(s)) and configure vertex attributes
+	// ------------------------------------------------------------------
 	float vertices[] = {
-		-0.5f, -0.5f, 0.0f, // left  
-		 0.5f, -0.5f, 0.0f, // right 
-		 0.0f,  0.5f, 0.0f  // top   
+		 0.5f,  0.5f, 0.0f,  // top right
+		 0.5f, -0.5f, 0.0f,  // bottom right
+		-0.5f, -0.5f, 0.0f,  // bottom left
+		-0.5f,  0.5f, 0.0f   // top left 
+	};
+	unsigned int indices[] = {  // note that we start from 0!
+		0, 1, 3,  // first Triangle
+		1, 2, 3   // second Triangle
 	};
 
 	glGenVertexArrays(1, &m_VAO);
 	glGenBuffers(1, &m_VBO);
+	glGenBuffers(1, &m_IBO);
 	// bind the Vertex Array Object first, then bind and set vertex buffer(s), and then configure vertex attributes(s).
 	glBindVertexArray(m_VAO);
 
 	glBindBuffer(GL_ARRAY_BUFFER, m_VBO);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, m_IBO);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
 	glEnableVertexAttribArray(0);
@@ -51,6 +62,8 @@ void SandboxLayer::OnDetach()
 	// Shutdown here
 	glDeleteVertexArrays(1, &m_VAO);
 	glDeleteBuffers(1, &m_VBO);
+	glDeleteBuffers(1, &m_IBO);
+	glDeleteShader(m_Shader->GetRendererID());
 }
 
 void SandboxLayer::OnEvent(Event& event)
@@ -66,10 +79,9 @@ void SandboxLayer::OnUpdate(Timestep ts)
 
 	// draw our first triangle
 	glUseProgram(m_Shader->GetRendererID());
-
 	glBindVertexArray(m_VAO); // seeing as we only have a single VAO there's no need to bind it every time, but we'll do so to keep things a bit more organized
-	glDrawArrays(GL_TRIANGLES, 0, 3);
-	
+	glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+	glBindVertexArray(0);
 }
 
 void SandboxLayer::OnImGuiRender()
