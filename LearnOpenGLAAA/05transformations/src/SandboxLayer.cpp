463e62b33693572cc148eca9fc6970840aaf2fd7
Jerry <306722860@qq.com>
2021-08-10 15:45:27 +0800

    Transformations.

---
+++ LearnOpenGLAAA/05transformations/src/SandboxLayer.cpp
@@ -0,0 +1,183 @@
+#include "SandboxLayer.h"
+#include "stb_image/stb_image.h"
+#include "glfw/include/GLFW/glfw3.h"
+
+using namespace GLCore;
+using namespace GLCore::Utils;
+
+SandboxLayer::SandboxLayer()
+{
+}
+
+SandboxLayer::~SandboxLayer()
+{
+}
+
+void SandboxLayer::OnAttach()
+{
+	EnableGLDebugging();
+
+	// Init here
+	m_Shader.push_back(Shader::FromGLSLTextFiles(
+		"assets/shaders/5.1.transform.vert.glsl",
+		"assets/shaders/5.1.transform.frag.glsl"
+	));
+
+	// set up vertex data (and buffer(s)) and configure vertex attributes
+	// ------------------------------------------------------------------
+	float triangle[] = {
+		 // positions       // texture coords
+		 0.5f,  0.5f, 0.0f, 1.0f, 1.0f, // top right
+		 0.5f, -0.5f, 0.0f, 1.0f, 0.0f, // bottom right
+		-0.5f, -0.5f, 0.0f, 0.0f, 0.0f, // bottom left
+		-0.5f,  0.5f, 0.0f, 0.0f, 1.0f  // top left 
+	};
+
+	unsigned int indices[] = {
+		0, 1, 3,
+		1, 2, 3
+	};
+
+	glGenVertexArrays(1, &m_VAO);
+	glGenBuffers(1, &m_VBO);
+	glGenBuffers(1, &m_IBO);
+
+	// triangle0 setup
+	glBindVertexArray(m_VAO);
+
+	glBindBuffer(GL_ARRAY_BUFFER, m_VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(triangle), triangle, GL_STATIC_DRAW);
+
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, m_IBO);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(0);
+
+	glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(3* sizeof(float)));
+	glEnableVertexAttribArray(1);
+
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+	glBindVertexArray(0);
+
+	stbi_set_flip_vertically_on_load(true);
+
+	// load and create a texture 
+	// -------------------------
+	glGenTextures(2, m_Texture);
+	glBindTexture(GL_TEXTURE_2D, m_Texture[0]); // all upcoming GL_TEXTURE_2D operations now have effect on this texture object
+	// set the texture wrapping parameters
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+	// set texture filtering parameters
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+	// load image, create texture and generate mipmaps
+	int width, height, nrChannels;
+	// The FileSystem::getPath(...) is part of the GitHub repository so we can find files on any IDE/platform; replace it with your own image path.
+	unsigned char* data = stbi_load("assets/textures/container.jpeg", &width, &height, &nrChannels, 0);
+	if (data)
+	{
+		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+		glGenerateMipmap(GL_TEXTURE_2D);
+	}
+	else
+	{
+		std::cout << "Failed to load texture" << std::endl;
+	}
+	stbi_image_free(data);
+
+	glBindTexture(GL_TEXTURE_2D, m_Texture[1]); // all upcoming GL_TEXTURE_2D operations now have effect on this texture object
+	// set the texture wrapping parameters
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);	// set texture wrapping to GL_REPEAT (default wrapping method)
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+	// set texture filtering parameters
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+	// load image, create texture and generate mipmaps
+	// The FileSystem::getPath(...) is part of the GitHub repository so we can find files on any IDE/platform; replace it with your own image path.
+	data = stbi_load("assets/textures/awesomeface.png", &width, &height, &nrChannels, 0);
+	if (data)
+	{
+		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
+		glGenerateMipmap(GL_TEXTURE_2D);
+	}
+	else
+	{
+		std::cout << "Failed to load texture" << std::endl;
+	}
+	stbi_image_free(data);
+
+	m_Shader[0]->use();
+	m_Shader[0]->setInt("texture1", 0);
+	m_Shader[0]->setInt("texture2", 1);
+}
+
+void SandboxLayer::OnDetach()
+{
+	// Shutdown here
+	glDeleteVertexArrays(1, &m_VAO);
+	glDeleteBuffers(1, &m_VBO);
+	glDeleteBuffers(1, &m_IBO);
+	for (auto m : m_Shader)
+	{
+		glDeleteShader(m->GetRendererID());
+	}
+}
+
+void SandboxLayer::OnEvent(Event& event)
+{
+	// Events here
+	EventDispatcher dispatcher(event);
+	dispatcher.Dispatch<KeyPressedEvent>(
+		[&](KeyPressedEvent& e)
+		{
+			if(e.GetKeyCode()==265)
+				m_mixF += 0.1;
+			if (e.GetKeyCode() == 264)
+				m_mixF -= 0.1;
+			return false;
+		});
+}
+
+void SandboxLayer::OnUpdate(Timestep ts)
+{
+	// Render here
+	glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+	glClear(GL_COLOR_BUFFER_BIT);
+
+	// bind Texture
+	glActiveTexture(GL_TEXTURE0);
+	glBindTexture(GL_TEXTURE_2D, m_Texture[0]);
+	glActiveTexture(GL_TEXTURE1);
+	glBindTexture(GL_TEXTURE_2D, m_Texture[1]);
+
+	// create transformations
+	glm::mat4 transform = glm::mat4(1.0f);
+	transform = glm::translate(transform, glm::vec3(0.5f, -0.5f, 0.0f));
+	transform = glm::rotate(transform, (float)glfwGetTime(), glm::vec3(0.0f, 0.0f, 1.0f));
+	transform = glm::scale(transform, glm::vec3(0.5, 0.5, 0.5));
+
+	glm::mat4 transform2 = glm::mat4(1.0f);
+	transform2 = glm::translate(transform2, glm::vec3(-0.5f, 0.5f, 0.0f));
+	transform2 = glm::scale(transform2, glm::vec3(1.0f, 1.0f, 1.0f)*sin((float)glfwGetTime()));
+
+	// get matrix's uniform location and set matrix
+	m_Shader[0]->use();
+	unsigned int transformLoc = glGetUniformLocation(m_Shader[0]->GetRendererID(), "transform");
+	glUniformMatrix4fv(transformLoc, 1, GL_FALSE, glm::value_ptr(transform));
+	
+	// draw triangle
+	glBindVertexArray(m_VAO); // seeing as we only have a single VAO there's no need to bind it every time, but we'll do so to keep things a bit more organized
+	glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+	glUniformMatrix4fv(transformLoc, 1, GL_FALSE, glm::value_ptr(transform2));
+	glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+
+	glBindVertexArray(0);
+}
+
+void SandboxLayer::OnImGuiRender()
+{
+	// ImGui here
+}
---
+++ LearnOpenGLAAA/05transformations/src/SandboxLayer.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#include <GLCore.h>
+#include <GLCoreUtils.h>
+
+class SandboxLayer : public GLCore::Layer
+{
+public:
+	SandboxLayer();
+	virtual ~SandboxLayer();
+
+	virtual void OnAttach() override;
+	virtual void OnDetach() override;
+	virtual void OnEvent(GLCore::Event& event) override;
+	virtual void OnUpdate(GLCore::Timestep ts) override;
+	virtual void OnImGuiRender() override;
+private:
+	std::vector<GLCore::Utils::Shader*> m_Shader;
+
+	GLuint m_VAO, m_VBO, m_IBO, m_Texture[2];
+	float m_mixF=0.2;
+};
