11d46539325ac367adb95aaf12bbb797e7ff313c
Jerry <306722860@qq.com>
2021-08-16 12:47:15 +0800

    Light Casters. dirctional light.

---
+++ LearnOpenGLAAA/12light_casters/src/SandboxLayer.h
@@ -0,0 +1,68 @@
+#pragma once
+
+#include <GLCore.h>
+#include <GLCoreUtils.h>
+
+
+class SandboxLayer : public GLCore::Layer
+{
+public:
+	SandboxLayer();
+	virtual ~SandboxLayer();
+
+	virtual void OnAttach() override;
+	virtual void OnDetach() override;
+	virtual void OnEvent(GLCore::Event& event) override;
+	virtual void OnUpdate(GLCore::Timestep ts) override;
+	virtual void OnImGuiRender() override;
+private:
+	std::vector<GLCore::Utils::Shader*> m_Shader;
+
+	GLuint m_CubeVAO, m_LightCubeVAO;
+	GLuint m_VBO, m_IBO;
+
+	float m_Fov = 45.0f;
+
+	float m_Width  = 800.0f;
+	float m_Height = 600.0f;
+
+	GLCore::Utils::FPSCamera m_Camera;
+	glm::vec3 m_CameraPos   = glm::vec3(0.0f, 0.0f, 3.0f);
+	glm::vec3 m_CameraFront = glm::vec3(0.0f, 0.0f, -1.0f);
+	glm::vec3 m_CameraUp    = glm::vec3(0.0f, 1.0f, 0.0f);
+	float m_CameraSpeed     = 0.05f;
+	float m_SpeedFactor     = 2.5f;
+
+	float m_DeltaTime = 0.0f;
+	float m_LastFrame = 0.0f;
+
+	float m_LastX = m_Width / 2;
+	float m_LastY = m_Height / 2;
+
+	float m_Yaw = 0.0f;
+	float m_Pitch = 0.0f;
+
+	bool m_FirstMouse = true;
+
+	glm::vec3 m_LightPos = glm::vec3(1.2f, 1.0f, 2.0f);
+
+	float m_Shininess = 32.0f;
+
+	std::shared_ptr<GLCore::Utils::Texture> m_ContainerTex;
+	std::shared_ptr<GLCore::Utils::Texture> m_ContainerSpecTex;
+	std::shared_ptr<GLCore::Utils::Texture> m_EmissiveTex;
+
+
+	std::vector<glm::vec3> cubePositions = {
+		glm::vec3(0.0f,  0.0f,  0.0f),
+		glm::vec3(2.0f,  5.0f, -15.0f),
+		glm::vec3(-1.5f, -2.2f, -2.5f),
+		glm::vec3(-3.8f, -2.0f, -12.3f),
+		glm::vec3(2.4f, -0.4f, -3.5f),
+		glm::vec3(-1.7f,  3.0f, -7.5f),
+		glm::vec3(1.3f, -2.0f, -2.5f),
+		glm::vec3(1.5f,  2.0f, -2.5f),
+		glm::vec3(1.5f,  0.2f, -1.5f),
+		glm::vec3(-1.3f,  1.0f, -1.5f)
+	};
+};
--- LearnOpenGLAAA/OpenGL-Core/src/GLCore/Util/Texture.cpp
+++ LearnOpenGLAAA/OpenGL-Core/src/GLCore/Util/Texture.cpp
@@ -9,39 +9,36 @@ namespace GLCore::Utils {
 	void Texture::loadTexture(char const * path)
 	{
 
-		glGenTextures(1, &m_RendererID);
+        glGenTextures(1, &m_RendererID);
 
-		glBindTexture(GL_TEXTURE_2D, m_RendererID); // all upcoming GL_TEXTURE_2D operations now have effect on this texture object
+        int width, height, nrComponents;
+        unsigned char* data = stbi_load(path, &width, &height, &nrComponents, 0);
+        if (data)
+        {
+            GLenum format;
+            if (nrComponents == 1)
+                format = GL_RED;
+            else if (nrComponents == 3)
+                format = GL_RGB;
+            else if (nrComponents == 4)
+                format = GL_RGBA;
 
-		// set the texture wrapping parameters
-		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
-		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-		// set texture filtering parameters
-		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
-		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
-		// load image, create texture and generate mipmaps
-		int width, height, nrChannels;
-		// The FileSystem::getPath(...) is part of the GitHub repository so we can find files on any IDE/platform; replace it with your own image path.
-		unsigned char* data = stbi_load(path, &width, &height, &nrChannels, 0);
-		if (data)
-		{
-			GLenum tformat;
-			if (nrChannels == 1)
-				tformat = GL_RED;
-			else if (nrChannels == 3)
-				tformat = GL_RGB;
-			else if (nrChannels == 4)
-				tformat = GL_RGBA;
-			glTexImage2D(GL_TEXTURE_2D, 0, tformat, width, height, 0, tformat, GL_UNSIGNED_BYTE, data);
-			glGenerateMipmap(GL_TEXTURE_2D);
-			LOG_INFO("Successd to load texture!");
+            glBindTexture(GL_TEXTURE_2D, m_RendererID);
+            glTexImage2D(GL_TEXTURE_2D, 0, format, width, height, 0, format, GL_UNSIGNED_BYTE, data);
+            glGenerateMipmap(GL_TEXTURE_2D);
 
-		}
-		else
-		{
-			LOG_INFO("Failed to load texture");
-		}
-		stbi_image_free(data);
+            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+            stbi_image_free(data);
+        }
+        else
+        {
+            std::cout << "Texture failed to load at path: " << path << std::endl;
+            stbi_image_free(data);
+        }
 	}
 
 	Texture::~Texture()
